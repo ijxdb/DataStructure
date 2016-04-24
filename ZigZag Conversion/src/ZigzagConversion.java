bd444d35bab62a567fc3705e34090766482419ac
Ankur Goyal <ankur3285@gmail.com>
2016-04-23 20:08:04 -0500

    Implemented the zig-zag functionality in O(n) time.

---
+++ ZigZag Conversion/src/ZigzagConversion.java
@@ -0,0 +1,44 @@
+
+public class ZigzagConversion {
+
+	public static void main(String[] args) {
+		
+		//input array example; input=1,2,3,4; output=1,3,2,4
+		int []arr = {4,2,3,4,7,6,5,9,1,2};
+		boolean flag=true;
+		
+		for(int i=1; i<arr.length; i++)
+		{	
+			
+
+				if (flag) /* "<" relation expected */
+				{
+					/* If we have a situation like A > B > C,
+					we get A > B < C by swapping B and C */
+					if (arr[i-1] > arr[i])
+						{	
+							int temp = arr[i-1];
+							arr[i-1]= arr[i];
+							arr[i]=temp;
+						}
+				}
+				else /* ">" relation expected */
+				{
+					/* If we have a situation like A < B < C,
+					we get A < C > B by swapping B and C */
+					if (arr[i-1] < arr[i])
+						{
+							int temp = arr[i-1];
+							arr[i-1]= arr[i];
+							arr[i]=temp;
+						}
+				}
+				flag = !flag; /* flip flag */
+			
+		}
+		
+		for(int i=0; i<arr.length; i++)
+		{System.out.print(arr[i]+" ");}
+	}
+
+}
